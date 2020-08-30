36b7c1ff1ea160745e2aebb8a45fbcc9eaeb6300
Sadia-web <sadiamim354@gmail.com>
2020-08-30 20:44:00 +0600

    router

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
+    //console.log(cart);
     //const total = cart.reduce((total, prd) => total + prd.price , 0)
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
@@ -35,7 +37,11 @@ const Cart = (props) => {
             <p>Product Price: {formatNumber(total)}</p>
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + VAT: {tax}</small></p>
-            <p>Total Price: {grandTotal}</p>           
+            <p>Total Price: {grandTotal}</p>
+            <br/>
+            <Link to="/review"> 
+            <button className="main-button">Review Order</button>
+            </Link>           
         </div>
     );
 };
